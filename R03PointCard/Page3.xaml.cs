7a15d11450c1a3ce182a702faa58646e8cc9c8d6
yoshzawa <yoshzawa@gmail.com>
2021-12-03 14:35:55 +0900

    add page3 content

---
+++ R03PointCard/Page3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace R03PointCard
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
