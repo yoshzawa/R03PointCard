ffb8784b4d50664462f25bbe00d29764d61ae5ec
yoshzawa <yoshzawa@gmail.com>
2021-12-03 14:06:48 +0900

    config

--- R03PointCard/App.xaml.cs
+++ R03PointCard/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace R03PointCard
 {
@@ -15,6 +18,10 @@ namespace R03PointCard
 
         protected override void OnStart()
         {
+            AppCenter.Start("ios=5b154fa1-c38a-4801-9816-b51a05488bea;" +
+                  "uwp={Your UWP App secret here};" +
+                  "android={Your Android App secret here}",
+                  typeof(Analytics), typeof(Crashes))
         }
 
         protected override void OnSleep()
