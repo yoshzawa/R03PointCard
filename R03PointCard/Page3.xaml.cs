446c62d41953976eb3a8c958e1c0a00b2997b986
yoshzawa <yoshzawa@gmail.com>
2021-12-03 15:15:24 +0900

    fix2

--- R03PointCard/Page3.xaml.cs
+++ R03PointCard/Page3.xaml.cs
@@ -15,10 +15,29 @@ namespace R03PointCard
         public Page3()
         {
             InitializeComponent();
+            if (Application.Current.Properties.ContainsKey("defaultUrl"))
+            {
+                bool defaultSw = (bool)Application.Current.Properties["defaultUrl"];
+                radioDefaultUrl.IsChecked = defaultSw;
+                radioDefaultUrl.IsChecked = !defaultSw;
+            }
+
         }
 
         private void Button1_Clicked(object sender, EventArgs e)
         {
+
+        }
+
+        private void radioDefaultUrl_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            Application.Current.Properties["defaultUrl"] = true;
+
+        }
+
+        private void radioCustomUrl_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            Application.Current.Properties["defaultUrl"] = false;
 
         }
     }
